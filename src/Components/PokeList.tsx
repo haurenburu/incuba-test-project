9384eb67c204cb6a1d9e39592da668b71d47f4f5
haurenburu <haurenburu@protonmail.com>
2021-01-04 12:04:51 -0300

    Add: grid to render first 20 Pokes from API

--- src/Components/PokeList.tsx
+++ src/Components/PokeList.tsx
@@ -3,19 +3,32 @@ import PokeCard from './PokeCard';
 
 import { Row } from 'reactstrap';
 
+interface IPokesResult extends Response {
+    results: Array<Object>
+}
+
 const PokeList: React.FC = () => {
+
+    const [pokes, setPokes] = React.useState<Array<Object>>([{}]);
+
+    React.useEffect(() => {
+        const func = async () => {
+            const res = await fetch('https://pokeapi.co/api/v2/pokemon');
+            const json: IPokesResult = await res.json();
+            // console.log(json.results);
+            setPokes(json.results);
+        }
+
+        func();
+
+    }, [])
+
     return (
         <>
-        <h1>PokeList</h1>
         <Row>
-            <PokeCard />
-            <PokeCard />
-            <PokeCard />
-            <PokeCard />
-            <PokeCard />
-            <PokeCard />
-            <PokeCard />
-            <PokeCard />
+            { pokes.map(( poke: any, index) => {
+                return <PokeCard key={poke.url + index + poke.name} url={poke.url} />
+            }) }
         </Row>
         </>
     )
